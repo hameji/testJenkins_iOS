5ff1c486b525f6974152459f8285498466fe4feb
Hajime Taniguchi <hameji@gmail.com>
2022-02-12 01:34:39 -0500

    プロジェクト作成に伴う生成ファイル

---
+++ testJenkins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testJenkins
+//
+//  Created by Hajime Taniguchi on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
